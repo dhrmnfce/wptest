3b348eab116904e4ee917e30a890aadd99305f2d
dhrmnfce <dylanludwick@gmail.com>
2018-10-15 02:41:27 +0000

    database details

--- wp-config.php
+++ wp-config.php
@@ -26,44 +26,44 @@ define('WP_MEMORY_LIMIT', '256M');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'c9');
+/*define('DB_NAME', 'c9');
 
 /** MySQL database username */
-define('DB_USER', substr(getenv('C9_USER'), 0, 16));
+/*define('DB_USER', substr(getenv('C9_USER'), 0, 16));
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+/*define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', getenv('IP'));
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-
-
-/* HEROKU DATABASE STUFF
-
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-/*define('DB_NAME', 'heroku_c82c25edb3ceb64');
-
-/** MySQL database username */
-/*define('DB_USER', 'b120d9bdabce05');
-
-/** MySQL database password */
-/*define('DB_PASSWORD', '99d9fbe0');
-
-/** MySQL hostname */
-/*define('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net');
+/*define('DB_HOST', getenv('IP'));
 
 /** Database Charset to use in creating database tables. */
 /*define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 /*define('DB_COLLATE', '');
+
+
+/* HEROKU DATABASE STUFF
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'heroku_c82c25edb3ceb64');
+
+/** MySQL database username */
+define('DB_USER', 'b120d9bdabce05');
+
+/** MySQL database password */
+define('DB_PASSWORD', '99d9fbe0');
+
+/** MySQL hostname */
+define('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
 
 
 
